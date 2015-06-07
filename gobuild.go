03991cdd85f84a7177cff8da3253e033ca0d3974
caixw <xnotepad@gmail.com>
2015-06-07 23:19:33 +0800

    删除不必要的空格

--- gobuild.go
+++ gobuild.go
@@ -27,10 +27,10 @@ import (
 const version = "0.2.5.150607"
 
 const usage = `gobuild 用于热编译Go程序。
- 
+
 命令行语法:
  gobuild [options] [dependents]
- 
+
  options:
   -h    显示当前帮助信息；
   -v    显示gobuild和go程序的版本信息；
@@ -38,19 +38,19 @@ const usage = `gobuild 用于热编译Go程序。
   -ext  需要监视的扩展名，默认值为"go"，区分大小写。
         若需要监视所有类型文件，请使用*，传递空值代表不监视任何文件；
   -main 指定需要编译的文件，默认为""。
- 
+
  dependents:
   指定其它依赖的目录，只能出现在命令的尾部。
- 
- 
+
+
 常见用法:
- 
+
  gobuild
    监视当前目录，若有变动，则重新编译当前目录下的*.go文件；
- 
+
  gobuild -main=main.go
    监视当前目录，若有变动，则重新编译当前目录下的main.go文件；
- 
+
  gobuild -main="main.go" dir1 dir2
    监视当前目录及dir1和dir2，若有变动，则重新编译当前目录下的main.go文件；
 `
