0f9ebeeba8e2ec161abfa306ec5cf1246db88e5e
caixw <xnotepad@gmail.com>
2016-11-29 23:45:42 +0800

    调整 build.sh 代码

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
 cd `dirname $0`
-
-go build -ldflags "-X main.buildDate=`date -u '+%Y%m%d'`  -X main.commitHash=`git rev-parse HEAD`" -v
+builddate=`date -u '+%Y%m%d'`
+commithash=`git rev-parse HEAD`
+go build -ldflags "-X main.buildDate=${builddate}  -X main.commitHash=${commithash}" -v
