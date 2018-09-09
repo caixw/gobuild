8a8c5800198fc5de85369cea74c365adf0b8fcd9
caixw <xnotepad@gmail.com>
2018-09-09 12:14:24 +0800

    添加对 go1.11 的支持

--- gobuild.go
+++ gobuild.go
@@ -7,7 +7,7 @@
 // 监视指定目录(可同时监视多个目录)下文件的变化，触发`go build`指令，
 // 实时编译指定的 Go 代码，并在编译成功时运行该程序。
 // 具体命令格式可使用`gobuild -h`来查看。
-package main
+package main // import "github.com/caixw/gobuild"
 
 import (
 	"flag"
