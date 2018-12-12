f80a58c02f95703b645edbbf84acbd6926f77979
caixw <xnotepad@gmail.com>
2018-12-12 21:25:05 +0800

    log 正确忽略 ignore

--- log.go
+++ log.go
@@ -29,17 +29,17 @@ const (
 
 // ConsoleLogs 将日志输出到控制台
 type ConsoleLogs struct {
-	Logs    chan *Log
-	ignore  bool // 是否忽略 ignore 通道的日志
-	writers map[int8]*logWriter
-	stop    chan struct{}
+	Logs       chan *Log
+	showIgnore bool
+	writers    map[int8]*logWriter
+	stop       chan struct{}
 }
 
 // NewConsoleLogs 声明 ConsoleLogs 实例
-func NewConsoleLogs(ignore bool) *ConsoleLogs {
+func NewConsoleLogs(showIgnore bool) *ConsoleLogs {
 	logs := &ConsoleLogs{
-		Logs:   make(chan *Log, 100),
-		ignore: ignore,
+		Logs:       make(chan *Log, 100),
+		showIgnore: showIgnore,
 		writers: map[int8]*logWriter{
 			LogTypeSuccess: newWriter(os.Stdout, colors.Green, "[SUCC]"),
 			LogTypeInfo:    newWriter(os.Stdout, colors.Blue, "[INFO]"),
@@ -63,6 +63,10 @@ func (logs *ConsoleLogs) output() {
 	for {
 		select {
 		case log := <-logs.Logs:
+			if !logs.showIgnore {
+				continue
+			}
+
 			w := logs.writers[log.Type]
 			colors.Fprint(w.out, w.color, colors.Default, w.prefix)
 			colors.Fprintln(w.out, colors.Default, colors.Default, log.Message)
