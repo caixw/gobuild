a73aea97a008513fea61c0fd82b952170f6783a5
caixw <xnotepad@gmail.com>
2019-03-07 22:38:34 +0800

    修正 windows 测试错误

--- gobuild_test.go
+++ gobuild_test.go
@@ -5,7 +5,8 @@
 package gobuild
 
 import (
-	"runtime"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -22,66 +23,55 @@ func TestGetExts(t *testing.T) {
 	a.Equal(getExts(",go , .php,"), []string{".go", ".php"})
 }
 
+func pathsEqual(a *assert.Assertion, paths1, paths2 []string) {
+	a.Equal(len(paths1), len(paths2))
+
+LOOP:
+	for _, p1 := range paths1 {
+		p1 = filepath.ToSlash(p1)
+		var p2 string
+		for _, p2 = range paths2 {
+			if p1 == p2 {
+				continue LOOP
+			}
+		}
+
+		a.True(false, "路径 %s %s 不相等", p1, p2)
+	}
+}
+
 func TestRecursivePath(t *testing.T) {
 	a := assert.New(t)
 
-	if runtime.GOOS != "windows" {
-		paths, err := recursivePaths(false, []string{"./testdir"})
-		a.NotError(err).
-			Equal(paths, []string{
-				"./testdir",
-			})
+	paths, err := recursivePaths(false, []string{"./testdir"})
+	a.NotError(err)
+	pathsEqual(a, paths, []string{
+		"./testdir",
+	})
 
-		paths, err = recursivePaths(true, []string{"./testdir"})
-		a.NotError(err).Equal(paths, []string{
-			"./testdir",
-			"testdir/testdir1",
-			"testdir/testdir2",
-			"testdir/testdir2/testdir3",
-		})
+	paths, err = recursivePaths(true, []string{"./testdir"})
+	a.NotError(err)
+	pathsEqual(a, paths, []string{
+		"./testdir",
+		"testdir/testdir1",
+		"testdir/testdir2",
+		"testdir/testdir2/testdir3",
+	})
 
-		paths, err = recursivePaths(true, []string{"./testdir/testdir1", "./testdir/testdir2"})
-		a.NotError(err).
-			Equal(paths, []string{
-				"./testdir/testdir1",
-				"./testdir/testdir2",
-				"testdir/testdir2/testdir3",
-			})
+	paths, err = recursivePaths(true, []string{"./testdir/testdir1", "./testdir/testdir2"})
+	a.NotError(err)
+	pathsEqual(a, paths, []string{
+		"./testdir/testdir1",
+		"./testdir/testdir2",
+		"testdir/testdir2/testdir3",
+	})
 
-		paths, err = recursivePaths(true, []string{"./testdir/testdir2"})
-		a.NotError(err).Equal(paths, []string{
-			"./testdir/testdir2",
-			"testdir/testdir2/testdir3",
-		})
-	} else {
-		paths, err := recursivePaths(false, []string{"./testdir"})
-		a.NotError(err).
-			Equal(paths, []string{
-				"./testdir",
-			})
-
-		paths, err = recursivePaths(true, []string{"./testdir"})
-		a.NotError(err).Equal(paths, []string{
-			"./testdir",
-			"testdir\\testdir1",
-			"testdir\\testdir2",
-			"testdir\\testdir2\\testdir3",
-		})
-
-		paths, err = recursivePaths(true, []string{"./testdir/testdir1", "./testdir/testdir2"})
-		a.NotError(err).
-			Equal(paths, []string{
-				".\\testdir\\testdir1",
-				".\\testdir\\testdir2",
-				"testdir\\testdir2\\testdir3",
-			})
-
-		paths, err = recursivePaths(true, []string{"./testdir/testdir2"})
-		a.NotError(err).Equal(paths, []string{
-			".\\testdir\\testdir2",
-			"testdir\\testdir2\\testdir3",
-		})
-	}
+	paths, err = recursivePaths(true, []string{"./testdir/testdir2"})
+	a.NotError(err)
+	pathsEqual(a, paths, []string{
+		"./testdir/testdir2",
+		"testdir/testdir2/testdir3",
+	})
 }
 
 func TestSplitArgs(t *testing.T) {
@@ -94,28 +84,14 @@ func TestSplitArgs(t *testing.T) {
 
 func TestGetAppName(t *testing.T) {
 	a := assert.New(t)
+	goexe := os.Getenv("GOEXE")
 
 	name, err := getAppName("", "./testdir")
-	a.NotError(err)
-	if runtime.GOOS != "windows" {
-		a.True(strings.HasSuffix(name, "testdir"), name)
-	} else {
-		a.True(strings.HasSuffix(name, "testdir.exe"), name)
-	}
+	a.NotError(err).True(strings.HasSuffix(name, "testdir"+goexe), name)
 
-	name, err = getAppName("a", "./testdir")
-	a.NotError(err)
-	if runtime.GOOS != "windows" {
-		a.True(strings.HasSuffix(name, "testdir/a"), name)
-	} else {
-		a.True(strings.HasSuffix(name, "testdir\\a.exe"), name)
-	}
+	name, err = getAppName("a", "./testdir/a")
+	a.NotError(err).True(strings.HasSuffix(name, "a"+goexe), name)
 
 	name, err = getAppName("a.exe", "./testdir")
-	a.NotError(err)
-	if runtime.GOOS != "windows" {
-		a.True(strings.HasSuffix(name, "testdir/a.exe"), name)
-	} else {
-		a.True(strings.HasSuffix(name, "testdir\\a.exe"), name)
-	}
+	a.NotError(err).True(strings.HasSuffix(name, "a.exe"), name)
 }
