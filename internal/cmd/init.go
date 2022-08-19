ebaf5c9fb6c3ff7081ef78ea8d47a02482d19220
caixw <xnotepad@gmail.com>
2022-08-19 18:44:15 +0800

    refactor(cmd): 当 init 没有指定参数直接在当前目录下创建文件

--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -3,15 +3,14 @@
 package cmd
 
 import (
-	"errors"
 	"flag"
 	"io"
 	"os"
+	"path/filepath"
 
+	"github.com/caixw/gobuild"
 	"github.com/issue9/cmdopt"
 	"golang.org/x/text/message"
-
-	i "github.com/caixw/gobuild/internal/init"
 )
 
 var initFS *flag.FlagSet
@@ -22,13 +21,19 @@ func initInit(o *cmdopt.CmdOpt, p *message.Printer) {
 
 func doInit(p *message.Printer) cmdopt.DoFunc {
 	return func(w io.Writer) error {
-		if initFS.NArg() == 0 {
-			return errors.New(p.Sprintf("未指定模块名称"))
-		}
 		wd, err := os.Getwd()
 		if err != nil {
 			return err
 		}
-		return i.Init(wd, initFS.Arg(0))
+
+		var name string
+		if initFS.NArg() > 0 {
+			name = initFS.Arg(0)
+		} else {
+			name = filepath.Base(wd) // 顺序不能乱，要先拿 name！
+			wd = filepath.Dir(wd)
+		}
+
+		return gobuild.Init(wd, name)
 	}
 }
